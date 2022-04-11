9e8cd7b087d83e71a95d209fd6ab45fff332cc47
yongu2000 <yongu200@hawaii.edu>
2022-04-12 03:39:44 +0900

    tts기능 추가
    영어 발음으로 나옴

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-const { Client, Intents } = require('discord.js'); 
+const { Client, Intents } = require('discord.js');
+const discordTTS = require('discord-tts');
 const client = new Client({
     intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.GUILD_MEMBERS]
 });
-const { joinVoiceChannel, createAudioPlayer, NoSubscriberBehavior, createAudioResource, AudioPlayerStatus, SubscriptionStatus } = require('@discordjs/voice');
+const { joinVoiceChannel, createAudioPlayer, NoSubscriberBehavior, AudioPlayer, StreamType, createAudioResource, VoiceConnectionStatus, entersState } = require('@discordjs/voice');
 const play = require('play-dl');
 
 const { token, youtube_key } = require('./config.json');
@@ -10,11 +11,18 @@ const Youtube = require('youtube-node');
 const youtube = new Youtube();
 youtube.setKey(youtube_key);
 
-const player = createAudioPlayer();
-
 client.on('ready', () => { 
     console.log(`Logged in as ${client.user.tag}`); 
 }); 
+
+let voiceConnection;
+let audioPlayer = new AudioPlayer();
+
+
+let yt_info;
+let stream;
+let resource;
+let player;
 
 client.on('messageCreate', async msg => { 
     if (msg.content.startsWith('!play')) {
@@ -25,25 +33,45 @@ client.on('messageCreate', async msg => {
             adapterCreator: msg.guild.voiceAdapterCreator
         })
         let args = msg.content.split('play')[1]
-        let yt_info = await play.search(args, {
+        yt_info = await play.search(args, {
             limit: 1
         })
-        let stream = await play.stream(yt_info[0].url)
-        let resource = createAudioResource(stream.stream, {
+        stream = await play.stream(yt_info[0].url)
+        resource = createAudioResource(stream.stream, {
             inputType: stream.type
         })
-        let player = createAudioPlayer({
+        player = createAudioPlayer({
             behaviors: {
                 noSubscriber: NoSubscriberBehavior.Play
             }
         })
         player.play(resource)
         msg.react('▶')
+        msg.reply(`${args} 불러드릴게요`)
         connection.subscribe(player)
     } else if (msg.content == '!stop') {
         player.stop()
         msg.react('⏹')
-        msg.reply('재생을 중지합니다!')
+        msg.reply('그만 부를게요')
+    }
+    if (msg.content.startsWith("`")) {
+        if (!msg.member.voice?.channel) return msg.channel.send('TTS를 재생하려면 음성 채널에 입장해주세요')
+        let args = msg.content.split('` ')[1]
+        const stream = discordTTS.getVoiceStream(args);
+        const audioResource=createAudioResource(stream, {inputType: StreamType.Arbitrary, inlineVolume:true});
+        if(!voiceConnection || voiceConnection?.status===VoiceConnectionStatus.Disconnected){
+            voiceConnection = joinVoiceChannel({
+                channelId: msg.member.voice.channelId,
+                guildId: msg.guildId,
+                adapterCreator: msg.guild.voiceAdapterCreator,
+            });
+            voiceConnection=await entersState(voiceConnection, VoiceConnectionStatus.Connecting, 5_000);
+        }
+        
+        if(voiceConnection.status===VoiceConnectionStatus.Connected){
+            voiceConnection.subscribe(audioPlayer);
+            audioPlayer.play(audioResource);
+        }
     }
 });
          
