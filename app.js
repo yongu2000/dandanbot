a40e4a85b180b69a5e66465f20f34c95bba2e049
yongu2000 <yongu200@hawaii.edu>
2022-04-11 14:33:04 +0900

    음악 재생

--- app.js
+++ app.js
@@ -1,15 +1,50 @@
 const { Client, Intents } = require('discord.js'); 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
-const token = require('./token.json');
+const client = new Client({
+    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.GUILD_MEMBERS]
+});
+const { joinVoiceChannel, createAudioPlayer, NoSubscriberBehavior, createAudioResource, AudioPlayerStatus, SubscriptionStatus } = require('@discordjs/voice');
+const play = require('play-dl');
+
+const { token, youtube_key } = require('./config.json');
+const Youtube = require('youtube-node');
+const youtube = new Youtube();
+youtube.setKey(youtube_key);
+
+const player = createAudioPlayer();
 
 client.on('ready', () => { 
     console.log(`Logged in as ${client.user.tag}`); 
 }); 
 
-client.on('message', msg => { 
-    if (msg.content === 'ping') {
-        msg.reply('Pong!'); 
-    } 
-}); 
+client.on('messageCreate', async msg => { 
+    if (msg.content.startsWith('!play')) {
+        if (!msg.member.voice?.channel) return msg.channel.send('노래를 재생하려면 음성 채널에 입장해주세요')
+        const connection = joinVoiceChannel({
+            channelId: msg.member.voice.channel.id,
+            guildId: msg.guild.id,
+            adapterCreator: msg.guild.voiceAdapterCreator
+        })
+        let args = msg.content.split('play')[1]
+        let yt_info = await play.search(args, {
+            limit: 1
+        })
+        let stream = await play.stream(yt_info[0].url)
+        let resource = createAudioResource(stream.stream, {
+            inputType: stream.type
+        })
+        let player = createAudioPlayer({
+            behaviors: {
+                noSubscriber: NoSubscriberBehavior.Play
+            }
+        })
+        player.play(resource)
+        msg.react('▶')
+        connection.subscribe(player)
+    } else if (msg.content == '!stop') {
+        player.stop()
+        msg.react('⏹')
+        msg.reply('재생을 중지합니다!')
+    }
+});
          
-client.login(token.token);
+client.login(token);
