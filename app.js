3b171fcad0f4c850201dd3395c8240564e556bbb
yongu2000 <yongu200@hawaii.edu>
2022-04-12 09:38:53 +0900

    tts 제거 및 주석 추가

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 const { Client, Intents } = require('discord.js');
-const discordTTS = require('discord-tts');
 const client = new Client({
     intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.GUILD_MEMBERS]
 });
-const { joinVoiceChannel, createAudioPlayer, NoSubscriberBehavior, AudioPlayer, StreamType, createAudioResource, VoiceConnectionStatus, entersState, AudioPlayerStatus } = require('@discordjs/voice');
+const { joinVoiceChannel, createAudioPlayer, NoSubscriberBehavior, createAudioResource, AudioPlayerStatus } = require('@discordjs/voice');
 const play = require('play-dl');
 const { token } = require('./config.json');
 const prefix = '단젤아';
@@ -12,24 +11,29 @@ client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}`); 
 }); 
 
-let voiceConnection;
-let audioPlayer = new AudioPlayer();
-
+//노래 큐
 const queue = new Map();
+
+//노래 재생용 변수
 let yt_info;
 let stream;
 let resource;
 let player;
 
+//명령
 client.on('messageCreate', async msg => { 
     if (msg.author.bot) return;
     const serverQueue = queue.get(msg.guild.id);
+
+    //노래 추가
     if (msg.content.startsWith(`${prefix}불러줘 `)) {
         console.log('불러줘')
         execute(msg, serverQueue, false);
+        // 노래 추가 띄어쓰기 버전
     } else if (msg.content.startsWith(`${prefix} 불러줘 `)) {
         console.log('불러줘 띄어쓰기')
         execute(msg, serverQueue, true);
+        // 노래 스킵
     } else if (msg.content === `${prefix}스킵` || msg.content === `${prefix} 스킵`) {
         if (!msg.member.voice?.channel) return msg.channel.send('노래를 스킵하려면 음성 채널에 입장해주세요')
         console.log('스킵')
@@ -42,6 +46,7 @@ client.on('messageCreate', async msg => {
                 msg.reply('다음 노래 부를게요')
             }
         }
+        // 노래 중단 (큐 전부 비움)
     } else if (msg.content === `${prefix}그만` || msg.content === `${prefix} 그만`) {
         if (!msg.member.voice?.channel) return msg.channel.send('노래를 멈추려면 음성 채널에 입장해주세요')
         console.log('그만')
@@ -53,18 +58,21 @@ client.on('messageCreate', async msg => {
         } else {
             msg.reply('그만 부를 노래가 없어요');
         }
+        // 노래 일시 정지
     } else if (msg.content === `${prefix}잠깐` || msg.content === `${prefix} 잠깐`) {
         if (!msg.member.voice?.channel) return msg.channel.send('노래를 일시 정지하려면 음성 채널에 입장해주세요')
         console.log('잠깐')
         player.pause()
         msg.react('⏸')
         msg.reply('잠깐 쉴게요')
+        // 노래 일시 정지 해제
     } else if (msg.content === `${prefix}다시` || msg.content === `${prefix} 다시`) {
         if (!msg.member.voice?.channel) return msg.channel.send('노래를 다시 재생하려면 음성 채널에 입장해주세요')
         console.log('다시')
         player.resume()
         msg.react('⏯')
         msg.reply('다시 부를게요')
+        // 노래 목록 출력
     } else if (msg.content === `${prefix}목록` || msg.content === `${prefix} 목록`) {
         console.log('목록')
         if (serverQueue) {
@@ -87,6 +95,7 @@ client.on('messageCreate', async msg => {
     //     serverQueue.connection.destroy();
     // }
 
+    // 명령어 설명
     if (msg.content === `${prefix}설명해` || msg.content === `${prefix} 설명해`) {
         console.log('설명')
         msg.reply(' 단젤아불러줘 [노래제목] - 노래 예약, 재생 \n단젤아스킵 - 노래 스킵 \n단젤아목록 - 예약 노래 목록 \n단젤아잠깐 - 노래 일시정지 \n단젤아다시 - 노래 일시정지 해제 \n단젤아그만 - 예약, 재생중인 노래 제거');
@@ -94,7 +103,7 @@ client.on('messageCreate', async msg => {
 
 });
 
-
+// 노래 추가 비동기 함수
 async function execute(msg, serverQueue, space) {
     if (!msg.member.voice?.channel) return msg.channel.send('노래를 재생하려면 음성 채널에 입장해주세요')
     let args = ''
@@ -151,6 +160,7 @@ async function execute(msg, serverQueue, space) {
     }
 }
 
+//노래 실행 비동기 함수
 async function playSong(guild, song) {
     const serverQueue = queue.get(guild.id);
 
